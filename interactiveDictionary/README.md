4b08d6bccebdccebaa80e5152e24bde33656c326
Gopikris <gopikrishna.83@gmail.com>
2021-07-29 13:14:58 +0200

    chore(PyProg): Added Google translationt to Swedish

---
+++ interactiveDictionary/README.md
@@ -0,0 +1,23 @@
+# Python Interactive Dictonary Program
+
+## Program that gives meaning, antonym, synonym and translates to Swedish using the Google translation
+
+### Use the following modules for Python dictionary and Translation
+``` 
+pip install PyDictionary
+```
+
+```
+pip3 install googletrans
+```
+
+### If the above code results in an error like this:
+```
+AttributeError: 'NoneType' object has no attribute 'group'
+```
+
+### Then you have to uninstall the current googletrans version and install the new one using the following commands:
+```
+$ pip3 uninstall googletrans
+$ pip3 install googletrans==3.1.0a0
+```
--- interactiveDictionary/interactiveDict.py
+++ interactiveDictionary/interactiveDict.py
@@ -1,23 +1,40 @@
 from PyDictionary import PyDictionary
+from googletrans import Translator, constants
+from pprint import pprint
 
+# Defines the meaning for the given word
 def word_meaning(word):
     dictionary = PyDictionary()
-    return dictionary.meaning(word)
+    return "The word does not exist. Please double check it" if dictionary.meaning(word) is None else str(', '.join(str(e) for e in dictionary.meaning(word).values()))
+    
 
+# Defines the synonym  for the given word
 def word_synonym(word):
     dictionary = PyDictionary()
-    return dictionary.synonym(word)
+    return "The word does not exist. Please double check it" if dictionary.synonym(word) is None else ', '.join(str(e) for e in dictionary.synonym(word))
+    
 
+# Defines the antonym for the given word
 def word_antonym(word):
     dictionary = PyDictionary()
-    return dictionary.antonym(word)
+    return "The word does not exist. Please double check it" if dictionary.antonym(word) is None else ', '.join(str(e) for e in dictionary.antonym(word))
+    
+
+# Translates the word into Swedish
+def word_translate(word):
+    translator = Translator()
+    translation = translator.translate(word, dest='sv')
+    # pprint(constants.LANGUAGES)
+    return print(f"Translation to Swedish: {translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
 
 word = input("Enter the word:")
-print("This word will return with meaning, synonym, antonynm")
+
+print("Entered word will return with meaning, synonym, antonynm")
 print("Meaning :", word_meaning(word))
 print("Synonym :", word_synonym(word))
 print("Antonym :", word_antonym(word))
 
+word_translate(word)
 #data = json.load(open("Utils/src/interactiveDictionary/data.json", "r"))
 # def translate(dictionary):
 #      #dictionary = dictionary
